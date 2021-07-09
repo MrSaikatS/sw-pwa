963a969963049405a3f15afe64510b8b6142d4ca
saikatever <saikatsardar5@gmail.com>
2021-07-09 21:06:18 +0530

    adding cache

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
+const staticCache = 'static-cache';
+const assets = [
+	'/',
+	'/assets/bootstrap.min.css',
+	'/assets/bootstrap.min.css.map',
+	'/assets/fav-logo.png',
+	'/assets/pwa.png',
+	'/assets/pwa512.png',
+	'/assets/script.js',
+	'/assets/style.css',
+	'/assets/sw-new-logo.png',
+	'/index.html',
+	'/about.html',
+	'/contact.html',
+	'/offline.html',
+];
+
 self.addEventListener('install', (e) => {
-	console.log('sw installed');
+	e.waitUntil(
+		caches.open(staticCache).then((cache) => {
+			cache.addAll(assets);
+		})
+	);
 });
 
 self.addEventListener('activate', (e) => {
 	console.log('sw activated');
+});
+
+self.addEventListener('fetch', (e) => {
+	e.respondWith(
+		caches.match(e.request).then((res) => {
+			return res || fetch(e.request);
+		})
+	);
 });
